f16066fb3e57408bde09fea747eb4a0742b6b635
adamkijak <adamkijak@o2.pl>
2011-02-03 20:42:18 +0100

    Upgrade to Rails 3.0.0

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -2,6 +2,6 @@
 
 # Your secret key for verifying the integrity of signed cookies.
 # If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random, 
+# Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-ActionController::Base.cookie_verifier_secret = 'b4363f7212e61bf366b6e6c0b4f4c856eea949e3444954006c6ba8b0a0652cd22452b80bfae19d80c09ae8380d7c2c20ac80781e7c7e2e3566bc69a8f07e2d30';
+CeeMeS::Application.config.secret_token = 'aa2c1d2b7fa5133f0087fe843532903ebd60356b6f5b2858ed280e66cec4d16e09ebb2566a2aa9c332eeb3da4f1dadd63f5775f2255e54554382d048f5f495d5'
